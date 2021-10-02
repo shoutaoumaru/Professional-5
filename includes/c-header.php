cff270752e95b97b7120a1476bc460abb4c87903
aikawatakuya <85001312+aikawatakuya@users.noreply.github.com>
2021-10-02 14:19:58 +0900

    professional-5 php化

---
+++ includes/c-header.php
@@ -0,0 +1,31 @@
+    <div class="mobile-menu">
+      <nav class="mobile-menu__nav">
+        <ul class="mobile-menu__list">
+          <li class="mobile-menu__item"><a class="animsition-link" href="/">ホーム</a></li>
+          <li class="mobile-menu__item"><a class="animsition-link" href="/company/company.html">事例案内</a></li>
+          <li class="mobile-menu__item"><a class="animsition-link" href="/business/business.html">取扱業務</a></li>
+          <li class="mobile-menu__item"><a class="animsition-link" href="/cace/cace.html">事例紹介</a></li>
+          <li class="mobile-menu__item"><a class="animsition-link" href="/news/news.html">お知らせ</a></li>
+          <li class="mobile-menu__item"><a class="animsition-link" href="/recruit/recruit.html">採用情報</a></li>
+        </ul>
+        <div class="contact-btn">
+          <ul class="contact-btn-list">
+            <li class="contact-btn-item">
+              <a href="tel:05031599527">
+                <span class="tel">お問い合わせ</span>
+              </a>
+            </li>
+            <li class="contact-btn-item">
+              <a href="mailto:info@riv-round.com">
+                <span class="mail">お問い合わせ</span>
+              </a>
+            </li>
+            <li class="contact-btn-item">
+              <a href="https://www.instagram.com/?hl=ja" target="_blank">
+                <span class="insta">インスタグラム</span>
+              </a>
+            </li>
+        </div>
+        </ul>
+      </nav>
+    </div>
