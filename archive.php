314533f22239a3454d235feabae3fb0ba24b85db
aikawatakuya <85001312+aikawatakuya@users.noreply.github.com>
2021-10-09 09:47:04 +0900

    includes 修正

--- archive.php
+++ archive.php
@@ -6,80 +6,13 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>お知らせ一覧 | 士業テンプレート4</title>
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/styles/vendor/animsition.min.css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/styles/vendor/bootstrap-reboot.css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/styles/vendor/animsition.min.css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/styles/style.css">
+  <?php get_header(); ?>
 </head>
 
 <body>
   <div id="main-wrapper" class="news-wrapper animsition">
-    <header id="header" class="p-header">
-      <div class="mobile-container">
-        <div class="logo__img">
-          <a href=" <?php echo esc_url( home_url('/')); ?>">
-            <div class="logo"></div>
-          </a>
-        </div>
-        <div class="mobile-button">
-          <button class="mobile-menu__btn">
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-        </div>
-      </div>
-      <!-- /.mobile-container -->
-      <div class="pc-container">
-        <div class=" logo__img">
-          <a class="animsition-link" href=" <?php echo esc_url( home_url('/')); ?>">
-            <div class="logo-pc logo"></div>
-          </a>
-        </div>
-        <nav class="pc-nav">
-          <ul class="pc-nav-list">
-            <li class="pc-nav-item">
-              <a class="pc-nav-link animsition-link" href=" <?php echo esc_url( home_url('/company')); ?>"><span>事務所案内</span>
-                <small>Company</small></a>
-            </li>
-            <li class="pc-nav-item">
-              <a class="pc-nav-link animsition-link"
-                href=" <?php echo esc_url( home_url('/business')); ?>"><span>取扱業務</span><small>business</small></a>
-            </li>
-            <li class="pc-nav-item">
-              <a class="pc-nav-link animsition-link" href=" <?php echo esc_url( home_url('/cace')); ?>"><span>事例紹介</span><small>Works</small></a>
-            </li>
-            <li class="pc-nav-item">
-              <a class="pc-nav-link animsition-link"
-                href=" <?php echo esc_url( home_url('/recruit')); ?>"><span>採用情報</span><small>Recruit</small></a>
-            </li>
-            <li class="pc-nav-item">
-              <a class="pc-nav-link animsition-link" href=" <?php echo esc_url( home_url('/news')); ?>"><span>お知らせ</span><small>News</small></a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <!-- /.pc-container -->
-      <!-- <div class="reserve-btn-js">
-        <div class="reserve-btn-js__inner">
-          <div class="reserve-btn-js__01">
-            <span>お</span>
-            <span>問</span>
-            <span>合</span>
-            <span>せ</span>
-          </div>
-          <div class="reserve-btn-js__02 js-left">
-            <div class="tel">
-              <a href="tel:092-686-7954">電話で<br>お問い合わせ</a>
-            </div>
-            <div class="net">
-              <a href="#">フォームで<br>お問い合わせ</a>
-            </div>
-          </div>
-        </div>
-      </div> -->
-      <!-- /.reserve-btn -->
-    </header>
+    <!-- header -->
+    <?php get_template_part('includes/c-header'); ?>  
     <!-- /.header -->
     <section class="c-topview">
       <div class="c-topview__img">
@@ -111,10 +44,9 @@
         <?php if( function_exists ("the_pagination") ) the_pagination(); ?>
       </div>
     </section>
-    <!-- /.c-news -->
-    <?php get_template_part('includes/c-footer'); ?>      
+    <!-- /.c-news -->  
     <!-- /.footer -->
-    <?php get_template_part('includes/c-header'); ?>     
+    <?php get_footer(); ?>  
     <!-- /.mobile-menu -->
   </div>
   <!-- /.main-wrapper -->
